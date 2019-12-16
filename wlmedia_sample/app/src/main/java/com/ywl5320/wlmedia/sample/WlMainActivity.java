2cea7f5227645a6493cf91498d2d91e7adc73615
ywl5320 <596353036@qq.com>
2019-12-16 20:32:47 +0800

    update v1.0.0

---
+++ wlmedia_sample/app/src/main/java/com/ywl5320/wlmedia/sample/WlMainActivity.java
@@ -0,0 +1,42 @@
+package com.ywl5320.wlmedia.sample;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WlMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 10);
+        }
+    }
+
+    public void play_normal(View view) {
+        Intent intent = new Intent(this, WlNormalPlayerActivity.class);
+        startActivity(intent);
+    }
+
+    public void play_encrypt(View view) {
+        Intent intent = new Intent(this, WlEncryptActivity.class);
+        startActivity(intent);
+    }
+
+    public void play_buffert(View view) {
+        Intent intent = new Intent(this, WlBufferActivity.class);
+        startActivity(intent);
+    }
+
+    public void play_audio(View view) {
+        Intent intent = new Intent(this, WlAudioActivity.class);
+        startActivity(intent);
+    }
+}
